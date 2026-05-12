f1695993b8705ae0f286f1dd5223dca76be5fa2a
Ggowthampatnaik <161194915+Ggowthampatnaik@users.noreply.github.com>
2026-05-13 01:04:09 +0530

    Add Streamlit app with Docker configuration

---
+++ app/streamlit_app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from test import greet
+print("Imported successfully")
+print(greet())
+st.set_page_config(
+    page_title="Docker Streamlit Demo",
+    page_icon="🚀",
+    layout="centered"
+)
+
+st.title("🚀 Welcome to Docker + Streamlit")
+st.write("This app is running inside a Docker container.")
+
+if st.button("Click Me"):
+    st.success("Hello from Streamlit running in Docker!")
