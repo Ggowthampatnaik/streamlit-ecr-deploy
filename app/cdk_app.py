4aa5d8113802baddf6c936b7214b29b38cb40128
Ggowthampatnaik <161194915+Ggowthampatnaik@users.noreply.github.com>
2026-05-13 01:37:06 +0530

    Update cdk_app.py

--- app/cdk_app.py
+++ app/cdk_app.py
@@ -1,7 +1,24 @@
-from aws_cdk import App, Stack
+#!/usr/bin/env python3
 
-app = App()
+import aws_cdk as cdk
+from streamlit_ecs_stack import StreamlitEcsStack
 
-Stack(app, "TestStack")
+app = cdk.App()
+
+env_name = app.node.try_get_context("env") or "dev"
+instance_count = int(app.node.try_get_context("instanceCount") or "1")
+image_uri = app.node.try_get_context("imageUri")
+
+StreamlitEcsStack(
+    app,
+    "StreamlitEcsStack",
+    env_name=env_name,
+    instance_count=instance_count,
+    image_uri=image_uri,
+    env=cdk.Environment(
+        account="493272324412",
+        region="us-east-1"
+    )
+)
 
 app.synth()
